621886a659deafa5848947ffddd163bd470ef3c5
Prafful Daga <prraful@gmail.com>
2020-03-18 12:17:18 +0530

    parent child relations in components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'firstangular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('firstangular');
+    expect(app.mytitle).toEqual('firstangular');
   });
 
   it('should render title', () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { FamilyComponent } from './myfamily/family/family.component';
+import { GrandparentComponent } from './myfamily/grandparent/grandparent.component';
+import { FatherComponent } from './myfamily/father/father.component';
+import { MotherComponent } from './myfamily/mother/mother.component';
+import { ChildComponent } from './myfamily/child/child.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    FamilyComponent,
+    GrandparentComponent,
+    FatherComponent,
+    MotherComponent,
+    ChildComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
