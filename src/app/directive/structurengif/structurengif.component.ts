d351bf00084f2404554f6534afaf8a925c7959a4
Prafful Daga <prraful@gmail.com>
2020-03-27 12:27:47 +0530

    ngFor and ngIf directives

--- src/app/directive/structurengif/structurengif.component.ts
+++ src/app/directive/structurengif/structurengif.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructurengifComponent implements OnInit {
 
+  display: boolean = true
+
+  toggle = function(){
+
+    if(this.display){
+        this.display = false
+    }else{
+      this.display=true
+    }
+    console.log("Current display value: " + this.display);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
